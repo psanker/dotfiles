43fd2f7271aacec14b4484e5e5fd264309b3f140
Patrick Anker <patricksanker@gmail.com>
2022-09-21 22:59:16 -0400

    point telescope to hidden files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -178,6 +178,32 @@ cmp.setup({
   },
 })
 
+require('telescope').setup({
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '--glob=!.git'
+    }
+  },
+  pickers = {
+    find_files = {
+      find_command = {
+        'rg',
+        '--files',
+        '--hidden',
+        '--glob=!.git'
+
+      }
+    }
+  }
+})
 require('telescope').load_extension('harpoon')
 
 -- Keymaps
