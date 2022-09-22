d2cad426bb2179f1ca9f2d4b7e032da2d733659d
Patrick Anker <patricksanker@gmail.com>
2022-09-21 20:49:50 -0400

    track R specific nvim config

---
+++ nvim/.config/nvim/ftplugin/r.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('i', 'Â', ' |> ')
+vim.keymap.set('n', '<Leader>pm', ':RSend targets::tar_make()<CR>')
+vim.keymap.set('n', '<Leader>pp', ':RSend source(here::here("projects/prelude.R"))<CR>')
