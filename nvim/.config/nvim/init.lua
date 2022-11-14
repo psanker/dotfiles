583a5fa35611b08e6eaa2edc3bff612338e0767f
Patrick Anker <patricksanker@gmail.com>
2022-11-13 21:00:11 -0500

    chore: yeah some more changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -31,6 +31,9 @@ paq {
         run = function() require('nvim-treesitter.install').update({ with_sync = true }) end };
     'SmiteshP/nvim-navic';
 
+    -- Languages
+    'wuelnerdotexe/vim-astro';
+
     -- Completion tools
     'hrsh7th/nvim-cmp';
 
@@ -44,6 +47,10 @@ paq {
     'hrsh7th/cmp-path';
     'hrsh7th/cmp-buffer';
     'hrsh7th/vim-vsnip';
+
+    -- Todos and troubleshooting:
+    'folke/trouble.nvim';
+    'folke/todo-comments.nvim';
 
     -- Mainly for inlay hints for Rust
     'simrat39/rust-tools.nvim';
@@ -76,7 +83,6 @@ set.smartindent = true
 
 set.cursorline = true
 set.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-set.cmdheight = 0
 
 -- Whitespace chars for diagnostics
 set.listchars:append { eol = '¬', tab = '>~', trail = '~', extends = '>', precedes = '<', space = '␣' }
@@ -85,6 +91,9 @@ vim.nohls = true
 -- Split defaults
 set.splitright = true
 set.splitbelow = true
+
+-- Astro setup
+g.astro_typescript = 'enable'
 
 --Set completeopt to have a better completion experience
 -- :help completeopt
@@ -124,78 +133,7 @@ require('nvim-treesitter.configs').setup({
     },
 })
 require('leap').add_default_mappings()
-
-local lsp_defaults = {
-    flags = {
-        debounce_text_changes = 150,
-    },
-    capabilities = require('cmp_nvim_lsp').update_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-    ),
-    on_attach = function(_, _)
-        vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
-    end
-}
-
-local lspconfig = require('lspconfig')
-local navic = require('nvim-navic')
-
-lspconfig.util.default_config = vim.tbl_deep_extend(
-    'force',
-    lspconfig.util.default_config,
-    lsp_defaults
-)
-
-lspconfig.r_language_server.setup({
-    on_attach = function(client, bufnr)
-        navic.attach(client, bufnr)
-    end,
-})
-lspconfig.sumneko_lua.setup({
-    on_attach = function(client, bufnr)
-        navic.attach(client, bufnr)
-    end,
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
-lspconfig.gopls.setup({
-    on_attach = function(client, bufnr)
-        navic.attach(client, bufnr)
-    end
-})
-lspconfig.taplo.setup({
-    on_attach = function(client, bufnr)
-        navic.attach(client, bufnr)
-    end
-})
-lspconfig.ltex.setup({})
-
-local rt = require('rust-tools')
-rt.setup({
-    server = {
-        on_attach = function(client, bufnr)
-            navic.attach(client, bufnr)
-        end,
-    },
-})
+require('todo-comments').setup({})
 
 local cmp = require('cmp')
 cmp.setup({
@@ -236,6 +174,99 @@ cmp.setup({
     },
 })
 
+-- Autocomplete settings
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'LspAttached',
+    desc = 'LSP actions',
+    callback = function()
+        local bufmap = function(mode, lhs, rhs)
+            local opts = { buffer = true }
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+    end
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lsp_defaults = {
+    flags = {
+        debounce_text_changes = 150,
+    },
+    capabilities = capabilities,
+    on_attach = function(_, _)
+        vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+    end
+}
+
+local lspconfig = require('lspconfig')
+local navic = require('nvim-navic')
+
+lspconfig.util.default_config = vim.tbl_deep_extend(
+    'force',
+    lspconfig.util.default_config,
+    lsp_defaults
+)
+
+lspconfig.astro.setup({})
+lspconfig.r_language_server.setup({
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+})
+lspconfig.sumneko_lua.setup({
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+lspconfig.gopls.setup({
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+})
+lspconfig.taplo.setup({
+    on_attach = function(client, bufnr)
+        navic.attach(client, bufnr)
+    end,
+    capabilities = capabilities,
+})
+lspconfig.ltex.setup({
+    capabilities = capabilities,
+})
+
+local rt = require('rust-tools')
+rt.setup({
+    server = {
+        on_attach = function(client, bufnr)
+            navic.attach(client, bufnr)
+        end,
+        capabilities = capabilities,
+    },
+})
+
 require('telescope').setup({
     defaults = {
         vimgrep_arguments = {
@@ -272,11 +303,22 @@ vim.keymap.set('n', '<Leader>c', '<cmd> source ~/.config/nvim/init.lua<CR>')
 -- better viewing after jumps
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-o>', '<C-o>zz')
+vim.keymap.set('n', '<C-i>', '<C-i>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 vim.keymap.set('n', '<Leader>x', '<cmd>! chmod +x %<CR>', { silent = true })
 vim.keymap.set('n', '{', '{zz')
 vim.keymap.set('n', '}', '}zz')
+
+-- Hide/show command line
+vim.keymap.set('n', '<Leader>cl', function()
+    if (vim.o.cmdheight == 0) then
+        vim.o.cmdheight = 1
+    else
+        vim.o.cmdheight = 0
+    end
+end)
 
 -- better yanking/pasting
 vim.keymap.set('x', '<Leader>p', '"_dP')
@@ -289,14 +331,14 @@ vim.keymap.set('n', '<Leader>Y', '"+Y', { noremap = false })
 vim.keymap.set('n', '<Leader>d', '"_d')
 vim.keymap.set('v', '<Leader>d', '"_d')
 
-vim.keymap.set('n', '<Leader>fr', function() vim.lsp.buf.formatting() end)
+vim.keymap.set('n', '<Leader>fr', function() vim.lsp.buf.format({ async = true }) end)
 
 -- Open up a damn terminal
-vim.keymap.set('n', '<Leader>tt', '<cmd>vs<cr><cmd>terminal<cr>')
+vim.keymap.set('n', '<Leader>T', '<cmd>vs<cr><cmd>terminal<cr>')
 
 vim.keymap.set('n', '<Leader>gg', '<cmd>LazyGit<CR>', { silent = true })
 
--- Telescope
+-- Telescope (f)
 vim.keymap.set('n', '<Leader>ff', '<cmd> lua require("telescope.builtin").find_files()<CR>')
 vim.keymap.set('n', '<Leader>fg', '<cmd> lua require("telescope.builtin").live_grep()<CR>')
 vim.keymap.set('n', '<Leader>fb', '<cmd> lua require("telescope.builtin").buffers()<CR>')
@@ -311,7 +353,7 @@ vim.keymap.set('n', '<Leader>fS', '<cmd> lua require("telescope.builtin").lsp_wo
 -- vim.keymap.set('i', '[', '[]<Esc>ha')
 -- vim.keymap.set('i', '{', '{}<Esc>ha')
 
--- Harpoon maps
+-- Harpoon (h) maps
 vim.keymap.set('n', '<Leader>hh', '<cmd> lua require("harpoon.ui").toggle_quick_menu()<CR>')
 vim.keymap.set('n', '<Leader>ha', '<cmd> lua require("harpoon.mark").add_file()<CR>')
 vim.keymap.set('n', '<Leader>hn', '<cmd> lua require("harpoon.ui").nav_next()<CR>')
@@ -321,6 +363,30 @@ vim.keymap.set('n', '<Leader>hq', '<cmd> lua require("harpoon.ui").nav_file(4)<C
 vim.keymap.set('n', '<Leader>hw', '<cmd> lua require("harpoon.ui").nav_file(3)<CR>')
 vim.keymap.set('n', '<Leader>he', '<cmd> lua require("harpoon.ui").nav_file(2)<CR>')
 vim.keymap.set('n', '<Leader>hr', '<cmd> lua require("harpoon.ui").nav_file(1)<CR>')
+
+-- Trouble (x) maps
+vim.keymap.set('n', '<Leader>xx', '<cmd> TroubleToggle<CR>')
+vim.keymap.set('n', '<Leader>xq', '<cmd> TroubleToggle quickfix<CR>')
+
+-- TODO commments (t) maps
+vim.keymap.set('n', '<Leader>tt', '<cmd> TodoTelescope<CR>')
+vim.keymap.set('n', ']t', function() require('todo-comments').jump_next() end, { desc = "Next TODO comment" })
+vim.keymap.set('n', '[t', function() require('todo-comments').jump_prev() end, { desc = "Previous TODO comment" })
+
+-- LSP maps (GENERAL)
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>') -- Jump to the definition
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>') -- Jump to the declaration
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>') -- Lists all implementations of symbol
+vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>') -- Jump to def of symbol
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>') -- List all references
+vim.keymap.set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>') -- Displays function signature
+vim.keymap.set('n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>') -- Renames all references of symbol
+vim.keymap.set('n', '<Leader>q', '<cmd>lua vim.lsp.buf.code_action()<cr>') -- Selects a code action
+vim.keymap.set('x', '<Leader>q', '<cmd>lua vim.lsp.buf.range_code_action()<cr>') -- "" for a range
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>') -- Show diagnostics in floating pane
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>') -- Go to prev diagnositc
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>') -- Go to next diagnostic
 
 -- Theme config
 set.background = 'dark'
@@ -345,52 +411,3 @@ vim.diagnostic.config({
 -- Show line diagnostics automatically in hover window
 set.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
--- Autocomplete settings
-vim.api.nvim_create_autocmd('User', {
-    pattern = 'LspAttached',
-    desc = 'LSP actions',
-    callback = function()
-        local bufmap = function(mode, lhs, rhs)
-            local opts = { buffer = true }
-            vim.keymap.set(mode, lhs, rhs, opts)
-        end
-
-        -- Displays hover information about the symbol under the cursor
-        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
-        -- Jump to the definition
-        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-
-        -- Jump to declaration
-        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-
-        -- Lists all the implementations for the symbol under the cursor
-        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-
-        -- Jumps to the definition of the type symbol
-        bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-
-        -- Lists all the references
-        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-
-        -- Displays a function's signature information
-        bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
-        -- Renames all references to the symbol under the cursor
-        bufmap('n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
-        -- Selects a code action available at the current cursor position
-        bufmap('n', '<Leader>q', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-        bufmap('x', '<Leader>q', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-
-        -- Show diagnostics in a floating window
-        bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-
-        -- Move to the previous diagnostic
-        bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-
-        -- Move to the next diagnostic
-        bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-    end
-})
