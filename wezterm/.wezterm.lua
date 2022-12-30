27ff38cb7564bd8d5ecea17cf88bde853881ef81
Patrick Anker <patricksanker@gmail.com>
2022-12-29 21:52:02 -0500

    feat: remove wezterm (long live kitty)

--- wezterm/.wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-
-return {
-  default_prog = { '/usr/local/bin/fish', '-l' },
-  font = wezterm.font('FiraCode Nerd Font', { italic = false }),
-  send_composed_key_when_left_alt_is_pressed = true,
-  color_scheme = "Gruber (base16)",
-  hide_tab_bar_if_only_one_tab = true,
-}
