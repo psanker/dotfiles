20a8a66245c6323bce0b554a5bc91c4bfa04d665
Patrick Anker <patricksanker@gmail.com>
2023-01-26 11:34:48 -0500

    feat: all the things
    I mean... so many

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,6 +1,9 @@
-require('psanker.writing')
+local writing = require('psanker.writing')
 
 local Remap = require("psanker.keymap")
 local nnoremap = Remap.nnoremap
 
+writing.setup()
+
 nnoremap('<Leader>tn', 'a<!--::--><Esc>2F:a')
+nnoremap('<Leader>kk', '<cmd>RowMovementToggle<CR>')
--- nvim/.config/nvim/lua/psanker/keymap.lua
+++ nvim/.config/nvim/lua/psanker/keymap.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function bind(op, outer_opts)
-    outer_opts = outer_opts or {noremap = true}
+    outer_opts = outer_opts or { noremap = true }
     return function(lhs, rhs, opts)
         opts = vim.tbl_extend("force",
             outer_opts,
@@ -11,10 +11,27 @@ local function bind(op, outer_opts)
     end
 end
 
-M.nmap = bind("n", {noremap = false})
+local function local_bind(op, outer_opts)
+    outer_opts = outer_opts or { noremap = true }
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.api.nvim_buf_set_keymap(0, op, lhs, rhs, opts)
+    end
+end
+
+M.nmap = bind("n", { noremap = false })
 M.nnoremap = bind("n")
 M.vnoremap = bind("v")
 M.xnoremap = bind("x")
 M.inoremap = bind("i")
+
+M.buf_nmap = local_bind("n", { noremap = false })
+M.buf_nnoremap = local_bind("n")
+M.buf_vnoremap = local_bind("v")
+M.buf_xnoremap = local_bind("x")
+M.buf_inoremap = local_bind("i")
 
 return M
