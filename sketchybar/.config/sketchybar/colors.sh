27738bef3cac57f4e4042f6b3ec20ef3daa69e8e
Patrick Anker <patricksanker@gmail.com>
2023-02-07 22:13:55 -0500

    feat: add Sketchybar

---
+++ sketchybar/.config/sketchybar/colors.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Color Palette
+export BLACK=0xff232136
+export WHITE=0xffe0def4
+export RED=0xffeb6f92
+export GREEN=0xff9ccfd8
+export BLUE=0xff3e8fb0
+export YELLOW=0xfff6c177
+export ORANGE=0xffea9a97
+export MAGENTA=0xffc4a7e7
+export GREY=0xff908caa
+export TRANSPARENT=0x00000000
+
+# General bar colors
+# export BAR_COLOR=0xcc24273a
+export BAR_COLOR=0x662a273f
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0xff393552
+export BACKGROUND_2=0xff44415a
+
+export POPUP_BACKGROUND_COLOR=$BLACK
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
