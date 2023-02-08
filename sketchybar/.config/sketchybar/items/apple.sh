f56ebe943fcc550e5738c943f3e045bfbb1a8e44
Patrick Anker <patricksanker@gmail.com>
2023-02-07 23:03:43 -0500

    feat: add Spicetify and fix some sketchybar colors

--- sketchybar/.config/sketchybar/items/apple.sh
+++ sketchybar/.config/sketchybar/items/apple.sh
@@ -7,7 +7,7 @@ sketchybar --add item           apple.logo left                             \
                                                                             \
            --set apple.logo     icon=$APPLE                                 \
                                 icon.font="$FONT:Black:16.0"                \
-                                icon.color=$GREEN                           \
+                                icon.color=$YELLOW                           \
                                 background.padding_right=25                 \
                                 label.drawing=off                           \
                                 click_script="$POPUP_CLICK_SCRIPT"          \
