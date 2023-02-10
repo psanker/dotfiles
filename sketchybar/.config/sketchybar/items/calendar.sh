b7fcf365c4a87069fdd4f166d9396ceccbcfeaff
Patrick Anker <patricksanker@gmail.com>
2023-02-10 09:59:12 -0500

    feat: add task item to info bar

--- sketchybar/.config/sketchybar/items/calendar.sh
+++ sketchybar/.config/sketchybar/items/calendar.sh
@@ -6,7 +6,6 @@ sketchybar --add item     calendar right                    \
                           icon.padding_right=0              \
                           label.width=50                    \
                           label.align=right                 \
-                          background.padding_left=15        \
-                          update_freq=30                    \
+                          update_freq=5                    \
                           script="$PLUGIN_DIR/calendar.sh"  \
                           click_script="$PLUGIN_DIR/zen.sh"
