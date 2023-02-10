75991437dba28f1066ff040e826e22a46c889a97
Patrick Anker <patricksanker@gmail.com>
2023-02-10 15:09:51 -0500

    feat: add current task to statusbar

--- sketchybar/.config/sketchybar/items/task.sh
+++ sketchybar/.config/sketchybar/items/task.sh
@@ -5,10 +5,14 @@ sketchybar --add item task right \
                       icon.font="$FONT:Black:12.0" \
                       icon.highlight=true \
                       icon.highlight_color=$YELLOW \
-                      icon.padding_left=15 \
-                      icon.padding_right=5 \
+                      icon.padding_left=10 \
+                      icon.padding_right=10 \
+                      label.padding_left=10 \
                       label.padding_right=15 \
                       label.align=right \
+                      label.background.color=$BACKGROUND_2 \
+                      label.background.height=26 \
+                      label.background.corner_radius=9 \
                       background.color=$BACKGROUND_1 \
                       background.height=26 \
                       script="$PLUGIN_DIR/task.sh" \
--- sketchybar/.config/sketchybar/plugins/task.sh
+++ sketchybar/.config/sketchybar/plugins/task.sh
@@ -1,10 +1,29 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 TW_COUNT=$(task +TODAY count)
+TW_ACTIVE=$(task "$(task +ACTIVE ids)" info | \
+           grep Description | \
+           head -n1 | \
+           awk '{ for (i=2; i<=NF; i++) printf("%s%s", $i, (i<NF ? OFS : ORS))}')
 
-if [ $TW_COUNT -gt 0 ]; then
-    sketchybar --set $NAME icon="􀷾" label="$TW_COUNT" width=dynamic
-else
-    sketchybar --set $NAME icon="" label="" width=0
+TW_ACTIVE_SIZE=${#TW_ACTIVE}
+TW_ACTIVE_MAX_LEN=35
+
+if [ "$TW_ACTIVE_SIZE" -gt "$TW_ACTIVE_MAX_LEN" ]; then
+    cut_size=$(( TW_ACTIVE_MAX_LEN - 3 ))
+   TW_ACTIVE="$(echo "$TW_ACTIVE" | cut -c -"$cut_size")..."
 fi
 
+if [ "$TW_COUNT" -gt 0 ]; then
+    label=""
+
+    if [ "$TW_ACTIVE_SIZE" -gt 0 ]; then
+        label="$TW_ACTIVE"
+    fi
+
+    sketchybar --set "$NAME" icon="􀷾 $TW_COUNT" \
+                             label="$label" \
+                             width=dynamic
+else
+    sketchybar --set "$NAME" icon="" label="" width=0
+fi
