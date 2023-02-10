3d934a1e264652556abc6a46f717e98b4509cb02
Patrick Anker <patricksanker@gmail.com>
2023-02-10 15:09:51 -0500

    feat: add current task to statusbar

--- sketchybar/.config/sketchybar/plugins/volume.sh
+++ sketchybar/.config/sketchybar/plugins/volume.sh
@@ -4,7 +4,7 @@ WIDTH=100
 
 volume_change() {
   INITIAL_WIDTH=$(sketchybar --query $NAME | jq ".icon.width")
-  if [ "$INITIAL_WIDTH" -eq "0" ]; then
+  if [ "$INITIAL_WIDTH" -eq 0 ]; then
     sketchybar --animate tanh 30 --set $NAME width=$WIDTH icon.width=$INFO 
   else
     sketchybar --set $NAME icon.width=$INFO width=$WIDTH
