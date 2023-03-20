b2cf2526a4ed745e58e492a7ea6b9411ad6d46f9
Patrick Anker <patricksanker@gmail.com>
2023-03-19 23:22:45 -0400

    docs: update README with setup instructions

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 | macOS (work) | ![Screenshot of my macOS workstation](./static/macos.png) |
 
 Just my dotfiles. Everything is managed with [GNU Stow](https://www.gnu.org/software/stow/).
+
+## Installation
+
+Depending on platform, these are the installation instructions.
+
+|  Platform | Install instructions |
+|:---------:|---------------------:|
+| ArcoLinux |    `./arch/setup.sh` |
+|     macOS |   `./macos/setup.sh` |
---
+++ macos/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+root=$(pwd)
+found=0
+
+while [ $found -eq 0 ]; do
+    if [[ $root = "/" ]]; then
+        break
+    fi
+
+    for file in $root/*; do
+        if [[ "$file" = "stow.sh" ]]; then
+            $found=1
+            break
+        fi
+    done
+done
+
+if [ ! $found -eq 1 ]; then
+    echo "Stow root not found"
+    exit 1
+fi
+
+# Stow dotfiles
+exec "$root/stow.sh"
+
+# Install software
+brew bundle install --file=~/.Brewfile
+
