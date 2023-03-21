e737eee83830da9c6f05cf6467d1d191a0bcab34
Patrick Anker <patricksanker@gmail.com>
2023-03-20 22:53:32 -0400

    fix: use Hasklig with proper symbol mapping

--- macos/update_brew.sh
+++ macos/update_brew.sh
@@ -8,17 +8,27 @@ while [ $found -eq 0 ]; do
         break
     fi
 
+    echo "Searching for stow.sh in '$root'.."
+
     for file in $root/*; do
-        if [[ "$file" = "stow.sh" ]]; then
-            $found=1
+        f_name=$(basename "$file")
+
+        if [ "$f_name" = "stow.sh" ]; then
+            found=1
             break
         fi
     done
+
+    if [ ! $found -eq 1 ]; then
+        root=$(dirname $root)
+    fi
 done
 
 if [ ! $found -eq 1 ]; then
     echo "Stow root not found"
     exit 1
+else
+    echo "Found stow root.."
 fi
 
-brew bundle dump && mv Brewfile "$root/brew/.Brewfile"
+brew bundle dump && mv "$(pwd)/Brewfile" "$root/macos/brew/.Brewfile"
