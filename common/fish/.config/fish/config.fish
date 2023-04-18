e7f07ef24602ab5f23f07d5ba514a94e1d701b72
Patrick Anker <patricksanker@gmail.com>
2023-04-18 12:05:12 -0400

    fix: actually fix the task counter my word

--- common/fish/.config/fish/config.fish
+++ common/fish/.config/fish/config.fish
@@ -52,7 +52,7 @@ if status is-interactive
     alias nnn="$HOME/.local/bin/nnn.sh -deaHQ -Pp"
 
     alias t="task"
-    alias tt="task +TODAY or +OVERDUE or scheduled.before:tomorrow"
+    alias tt="task +TODAY or +OVERDUE or \(+SCHEDULED and due.after:today\)"
     alias ta="task add"
     alias td="task $1 done"
     alias tm="task $1 modify"
