6042c39f7e2815010d439bb023589920e6de3e6d
Patrick Anker <patricksanker@gmail.com>
2023-07-13 17:13:30 -0400

    kitty -> wezterm

---
+++ common/wez/.config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wez = require('wezterm')
+
+local cfg = {}
+
+-- Window
+cfg.enable_tab_bar = false
+cfg.window_padding = {
+    left = '2cell',
+    right = '2cell',
+    top = '1cell',
+    bottom = '0cell',
+}
+cfg.window_background_opacity = 0.97
+
+-- Font
+cfg.font = wez.font 'Hasklug Nerd Font'
+
+-- Theme
+cfg.color_scheme = 'rose-pine-moon'
+
+return cfg
