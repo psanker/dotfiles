6d11ed93affbaf81a47620d19602c9e8587961fa
Patrick Anker <patricksanker@gmail.com>
2023-07-24 14:06:29 -0400

    fix(nvim): remove normal mode BG
    Kitty apparently did this for Neovim automatically, but WezTerm does not

--- common/nvim/.config/nvim/lua/psanker/plugins/theme.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/theme.lua
@@ -13,6 +13,7 @@ return {
             })
 
             vim.cmd.colorscheme('rose-pine')
+            vim.cmd [[ hi Normal guibg=NONE ctermbg=NONE ]]
         end,
         priority = 1000,
         lazy = false,
