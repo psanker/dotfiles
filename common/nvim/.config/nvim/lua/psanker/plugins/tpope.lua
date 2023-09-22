7b9999a89f0b8c2e63412dea9adbdbbc48c8eae1
Patrick Anker <patricksanker@gmail.com>
2023-09-22 13:58:41 -0400

    feat(nvim): add dadbod
    Dadbod is yet another amazing tpope plugin, this time for database
    connections and queries. Daddob, on its own, is just text commands,
    so dadbod-ui creates the UI for Dadbod.
    
    * '<Leader>vd' toggles dadbod visibility
    * Comes with completion hooks for nvim-cmp

--- common/nvim/.config/nvim/lua/psanker/plugins/tpope.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/tpope.lua
@@ -15,4 +15,23 @@ return {
         'tpope/vim-fugitive',
         cmd = 'Git'
     },
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod',                     lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+        keys = {
+            { '<Leader>vd', '<cmd>DBUIToggle<CR>', desc = '[v]iew: [d]adbod-ui' }
+        },
+    },
 }
