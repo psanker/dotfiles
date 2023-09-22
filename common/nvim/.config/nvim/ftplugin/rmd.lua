4be32ffcf39bb6f0b1bc9a4c41ba4b1723fb2198
Patrick Anker <patricksanker@gmail.com>
2023-09-22 14:02:20 -0400

    fix(nvim): use 2-space tabs for Rmd files

--- common/nvim/.config/nvim/ftplugin/rmd.lua
+++ common/nvim/.config/nvim/ftplugin/rmd.lua
@@ -7,3 +7,7 @@ writing.setup()
 
 nnoremap('<Leader>tn', 'a<!--::--><Esc>2F:a')
 nnoremap('<Leader>kk', '<cmd>RowMovementToggle<CR>')
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
