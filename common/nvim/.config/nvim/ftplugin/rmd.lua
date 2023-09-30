0c6b704fa7397045548f36b0a5a9b01cbe11c521
Patrick Anker <patricksanker@gmail.com>
2023-09-30 11:51:40 -0400

    chore: some QoL improvements
    - Fix the lua_language_server message
    - Add RSend keybinds to Rmd type
    - Fix wezterm not using fish shell with neomutt (Mac)

--- common/nvim/.config/nvim/ftplugin/rmd.lua
+++ common/nvim/.config/nvim/ftplugin/rmd.lua
@@ -11,3 +11,8 @@ nnoremap('<Leader>kk', '<cmd>RowMovementToggle<CR>')
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+
+vim.keymap.set('i', '<C-P>', ' |> ')
+vim.keymap.set('n', '<Leader>pm', ':RSend targets::tar_make()<CR>')
+vim.keymap.set('n', '<Leader>pv', ':RSend targets::tar_visnetwork()<CR>')
+vim.keymap.set('n', '<Leader>pp', ':RSend source(here::here("projects/prelude.R"))<CR>')
