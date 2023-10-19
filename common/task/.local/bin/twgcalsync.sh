2975ff3012446eb5aa502c7039caccf0eb52c5a1
Patrick Anker <patricksanker@gmail.com>
2023-10-19 16:38:29 -0400

    ref: remove task config from public repo

--- common/task/.local/bin/twgcalsync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/bin/tw_gcal_sync -c tw -t sync --google-secret ~/.local/share/task/tw-gcal-sync.json --prefer-scheduled-date
-/usr/local/bin/tw_gcal_sync -c tw-home -t home --google-secret ~/.local/share/task/tw-gcal-sync.json --prefer-scheduled-date
-/usr/local/bin/tw_gcal_sync -c tw-work -t work --google-secret ~/.local/share/task/tw-gcal-sync.json --prefer-scheduled-date
