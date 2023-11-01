a45819d27cc72c2819188b6632dae4118dd866df
Patrick Anker <patricksanker@gmail.com>
2023-11-01 14:09:55 -0400

    fix: fix Nvim-R weirdness

--- common/nvim/.config/nvim/lua/psanker/plugins/langs.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/langs.lua
@@ -2,6 +2,9 @@ return {
     {
         'jalvesaq/Nvim-R',
         ft = { 'r', 'rmd' },
+        -- Something in the newest version of Nvim-R may be broken?
+        commit = "97601b006d8c572fd9c3cf33e2e8d20b6e5f81e8",
+        lazy = false,
     },
     {
         "iamcco/markdown-preview.nvim",
