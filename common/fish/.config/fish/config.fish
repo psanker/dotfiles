a4188070f964a667d86198a35bc5160fd83de53e
Patrick Anker <patrickanker@10-20-240-189.dynapool.wireless.nyu.edu>
2023-12-04 16:17:01 -0500

    feat: lmao old laptop died

--- common/fish/.config/fish/config.fish
+++ common/fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ fish_vi_key_bindings
 
 fish_add_path /usr/local/bin
 fish_add_path /usr/local/go/bin
+fish_add_path /opt/homebrew/bin
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/go/bin"
 fish_add_path "$HOME/.cargo/bin"
--- common/wez/.config/wezterm/wezterm.lua
+++ common/wez/.config/wezterm/wezterm.lua
@@ -2,6 +2,25 @@ local wez = require('wezterm')
 
 local cfg = {}
 
+-- From https://stackoverflow.com/questions/1340230/check-if-directory-exists-in-lua
+--- Check if a file or directory exists in this path
+function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok, err
+end
+
+--- Check if a directory exists in this path
+function isdir(path)
+   -- "/" works on both Unix and Windows
+   return exists(path.."/")
+end
+
 -- Window
 cfg.enable_tab_bar = false
 cfg.window_padding = {
@@ -20,6 +39,12 @@ cfg.font = wez.font 'Hasklug Nerd Font'
 cfg.color_scheme = 'rose-pine-moon'
 
 -- Shell
-cfg.default_prog = { '/usr/local/bin/fish', '-l' }
+local prefix = '/usr/local'
+
+if isdir('/opt/homebrew') then
+    prefix = '/opt/homebrew'
+end
+
+cfg.default_prog = { prefix .. '/bin/fish', '-l' }
 
 return cfg
