7cc11e68ebdb764a3c2864161f470958bd8dac40
Patrick Anker <patricksanker@gmail.com>
2024-01-22 13:50:19 -0500

    fix(nvim): allow quarto to have easier line jumps

--- common/nvim/.config/nvim/ftplugin/quarto.lua
+++ common/nvim/.config/nvim/ftplugin/quarto.lua
@@ -1,3 +1,11 @@
+local Remap = require("psanker.keymap")
+local nnoremap = Remap.nnoremap
+
+
+require('psanker.edit.writing').setup()
+
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+
+nnoremap('<Leader>kk', '<cmd>RowMovementToggle<CR>')
