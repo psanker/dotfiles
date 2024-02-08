a320631411c98e5e9e6fe83ce7fefa17d8f11d92
Patrick Anker <patricksanker@gmail.com>
2024-02-07 23:59:08 -0500

    feat(fish): add cabal to PATH

--- common/fish/.config/fish/config.fish
+++ common/fish/.config/fish/config.fish
@@ -6,6 +6,7 @@ fish_add_path /opt/homebrew/bin
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/go/bin"
 fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/.cabal/bin"
 
 if test (uname) = "Darwin"
     fish_add_path "$(brew --prefix)/opt/python3/libexec/bin"
