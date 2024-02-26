2a993477d3e9f97a4ceb4fda2f73177150b775f4
Patrick Anker <patricksanker@gmail.com>
2024-02-26 17:10:24 -0500

    chore(nvim): move Git keybinds over to plugin spec

--- common/nvim/.config/nvim/after/plugin/keymap.lua
+++ common/nvim/.config/nvim/after/plugin/keymap.lua
@@ -8,7 +8,6 @@ local xnoremap = Remap.xnoremap
 -- 0. General --
 nnoremap('<Leader>bd', ':%bd|e#<CR>|:bd#<CR>')
 nnoremap('<Leader>bp', '<cmd> echo expand("%:p")<CR>')
-nnoremap('|', ':!')
 nnoremap('XX', '<cmd>call delete(@%) | bdelete!<CR><cmd>qa!<CR>') -- Equivalent to ZZ but for deletion of file
 
 -- better viewing after jumps
@@ -46,24 +45,17 @@ vnoremap('<Leader>d', '"_d')
 vnoremap('J', ":m '>+1<CR>gv=gv")
 vnoremap('K', ":m '<-2<CR>gv=gv")
 
--- 1. Git --
-nnoremap('<Leader>gg', '<cmd>Git<CR>', { silent = true, desc = 'Open fu[g]itive window' })
-nnoremap('<Leader>gp', '<cmd>Git pull<CR>', { silent = true, desc = '[g]it [p]ull' })
-nnoremap('<Leader>gP', '<cmd>Git push<CR>', { silent = true, desc = '[g]it [P]ush' })
-nnoremap('<Leader>gl', '<cmd>Git log<CR>', { desc = '[g]it [l]og' })
-nnoremap('<Leader>gf', '<cmd>Git fetch<CR>', { desc = '[g]it [f]etch' })
-
--- 2. View different windows
+-- 1. View different windows
 nnoremap('<Leader>vo', '<cmd>AerialToggle<CR>', { desc = '[v]iew [o]utline' })
 nnoremap('<Leader>vt', '<cmd>TodoTrouble<CR>', { desc = '[v]iew [t]odos' })
 nnoremap('<Leader>vT', '<cmd>TodoTelescope<CR>', { desc = '[v]iew [T]odos in Telescope' })
 
--- 3. Quickfix commands
+-- 2. Quickfix commands
 nnoremap('<Leader>vx', '<cmd>TroubleToggle<CR>', { desc = '[v]iew quickfi[x]' })
 nnoremap(']x', '<cmd>cnext<CR>', { desc = 'Go to next quickfi[x]' })
 nnoremap('[x', '<cmd>cprevious<CR>', { desc = 'Go to previous quickfi[x]' })
 
--- 4. Note capture and quick actions (q) --
+-- 3. Note capture and quick actions (q) --
 nnoremap('<Leader>qn', '<cmd>FloatermNew! --cwd=~/personal/pkm ~/.local/bin/quick-note.sh<CR>',
     { desc = 'Open a [q]uick note in a floating term' })
 nnoremap('<Leader>qj', '<cmd>FloatermNew! --cwd=~/personal/pkm ~/.local/bin/quick-journal.sh<CR>',
--- common/nvim/.config/nvim/lua/psanker/plugins/tpope.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/tpope.lua
@@ -15,8 +15,33 @@ return {
         'tpope/vim-fugitive',
         cmd = 'Git',
         keys = {
-            { '<Leader>gdv', '<cmd>Gvdiffsplit!<CR>', desc = '[g]it: open 3-way [d]iff [v]ertical split' },
-            { '<Leader>gdh', '<cmd>Gdiffsplit!<CR>', desc = '[g]it: open 3-way [d]iff [h]orizontal split' },
+            {
+                '<Leader>gg', '<cmd>Git<CR>', silent = true, desc = 'Open fu[g]itive window'
+            },
+            {
+                '<Leader>gp', '<cmd>Git pull<CR>', silent = true, desc = '[g]it [p]ull'
+            },
+            {
+                '<Leader>gP', '<cmd>Git push<CR>', silent = true, desc = '[g]it [P]ush'
+            },
+            {
+                '<Leader>gl', '<cmd>Gclog!<CR>', desc = 'Load [g]it [l]og into quickfix'
+            },
+            {
+                '<Leader>gl',
+                ":'<,'>Gclog!<CR>",
+                mode = 'v',
+                desc = 'Load [g]it [l]og for current hunk into quickfix',
+            },
+            {
+                '<Leader>gf', '<cmd>Git fetch<CR>', desc = '[g]it [f]etch'
+            },
+            {
+                '<Leader>gdv', '<cmd>Gvdiffsplit!<CR>', desc = '[g]it: open 3-way [d]iff [v]ertical split'
+            },
+            {
+                '<Leader>gdh', '<cmd>Gdiffsplit!<CR>', desc = '[g]it: open 3-way [d]iff [h]orizontal split'
+            },
         },
     },
     {
@@ -35,7 +60,7 @@ return {
             vim.g.db_ui_use_nerd_fonts = 1
         end,
         keys = {
-            { '<Leader>vd', '<cmd>DBUIToggle<CR>', desc = '[v]iew: [d]adbod-ui' }
+            { '<Leader>vd', '<cmd>DBUIToggle<CR>', desc = '[v]iew: [d]adbod-ui' },
         },
     },
 }
