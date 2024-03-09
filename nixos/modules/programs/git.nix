18845a3bea72f4d3811d28670a38d7524dfb010c
Patrick Anker <patricksanker@gmail.com>
2024-03-09 17:22:37 -0500

    feat: add GPG signing to git

--- nixos/modules/programs/git.nix
+++ nixos/modules/programs/git.nix
@@ -1,16 +1,35 @@
-{vars, ...}: {
+{
+  pkgs,
+  vars,
+  lib,
+  ...
+}: {
+  options = {
+    myopts.programs.git.enable = with lib;
+      mkOption {
+        type = types.bool;
+        default = false;
+        example = true;
+        description = "Whether to enable the git module";
+      };
+  };
+
   config = {
-    programs.git = {
-      enable = true;
+    myopts.programs.git.enable = true;
 
-      config = {
-        init = {
-          defaultBranch = "main";
-        };
+    home-manager.users.${vars.user} = {
+      programs.git = {
+        enable = true;
 
-        user = {
-          inherit (vars) name;
-          inherit (vars) email;
+        package = pkgs.git;
+
+        userName = vars.name;
+        userEmail = vars.email;
+
+        extraConfig = {
+          init = {
+            defaultBranch = "main";
+          };
         };
       };
     };
--- nixos/modules/programs/gnupg.nix
+++ nixos/modules/programs/gnupg.nix
@@ -1,4 +1,19 @@
-{config, lib, vars, ...}: {
+{
+  config,
+  lib,
+  vars,
+  ...
+}: {
+  options = {
+    myopts.programs.gpg.enable = with lib;
+      mkOption {
+        type = types.bool;
+        default = false;
+        example = true;
+        description = "Whether Home Manager controls GPG";
+      };
+  };
+
   config = let
     hmcfg = config.home-manager.users.${vars.user};
     usingLinux = config.myopts.platform.linux;
@@ -8,17 +23,28 @@
       else "${hmcfg.home.homeDirectory}/.config";
     sopsSecrets = config.sops.secrets;
   in {
+    myopts.programs.gpg.enable = true;
+
     home-manager.users.${vars.user} = {
       programs.gpg = {
         enable = true;
         homedir = "${xdgConfigHome}/gnupg";
       };
+
+      programs.git.signing =
+        lib.mkIf config.myopts.programs.git.enable {
+          key = "BF9AB14D3B994BB1";
+          signByDefault = true;
+        };
     };
 
     systemd.user.services.gpgimporter = lib.mkIf usingLinux {
       script = ''
         cat ${sopsSecrets."gpg/keyring".path} | base64 -d | ${hmcfg.programs.gpg.package}/bin/gpg --import
       '';
+      serviceConfig = {
+        Type = "oneshot";
+      };
     };
   };
 }
