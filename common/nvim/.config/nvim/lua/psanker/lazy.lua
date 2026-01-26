d8932784a0b7708f187de2351a499b4aacc1e998
Patrick Anker <patricksanker@gmail.com>
2026-01-26 16:49:50 -0500

    ref: move treesitter config into Lazy

--- common/nvim/.config/nvim/lua/psanker/lazy.lua
+++ common/nvim/.config/nvim/lua/psanker/lazy.lua
@@ -21,68 +21,68 @@ require('lazy').setup({
 })
 
 -- Post Lazy weirdness (mainly just treesitter)
-require('nvim-treesitter.config').setup({
-    auto_install = true,
-    sync_install = false,
-    ignore_install = {},
-    ensure_installed = {
-        'r', 'rust', 'markdown',
-        'markdown_inline', 'python',
-        'go', 'ocaml', 'gitcommit'
-    },
-    highlight = {
-        enable = true,
-        disable = { 'tsql' },
-    },
-    textobjects = {
-        move = {
-            enable = true,
-            set_jumps = true,
+-- require('nvim-treesitter.config').setup({
+--     auto_install = true,
+--     sync_install = false,
+--     ignore_install = {},
+--     ensure_installed = {
+--         'r', 'rust', 'markdown',
+--         'markdown_inline', 'python',
+--         'go', 'ocaml', 'gitcommit'
+--     },
+--     highlight = {
+--         enable = true,
+--         disable = { 'tsql' },
+--     },
+--     textobjects = {
+--         move = {
+--             enable = true,
+--             set_jumps = true,
 
-            goto_next_start = {
-                ["]p"] = "@parameter.inner",
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[p"] = "@parameter.inner",
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-            },
-        },
-        lsp_interop = {
-            enable = true,
-            border = 'none',
-            peek_definition_code = {
-                ["<leader>df"] = "@function.outer",
-                ["<leader>dF"] = "@class.outer",
-            },
-        },
-        select = {
-            enable = true,
-            lookahead = true,
+--             goto_next_start = {
+--                 ["]p"] = "@parameter.inner",
+--                 ["]m"] = "@function.outer",
+--                 ["]]"] = "@class.outer",
+--             },
+--             goto_next_end = {
+--                 ["]M"] = "@function.outer",
+--                 ["]["] = "@class.outer",
+--             },
+--             goto_previous_start = {
+--                 ["[p"] = "@parameter.inner",
+--                 ["[m"] = "@function.outer",
+--                 ["[["] = "@class.outer",
+--             },
+--             goto_previous_end = {
+--                 ["[M"] = "@function.outer",
+--                 ["[]"] = "@class.outer",
+--             },
+--         },
+--         lsp_interop = {
+--             enable = true,
+--             border = 'none',
+--             peek_definition_code = {
+--                 ["<leader>df"] = "@function.outer",
+--                 ["<leader>dF"] = "@class.outer",
+--             },
+--         },
+--         select = {
+--             enable = true,
+--             lookahead = true,
 
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
+--             keymaps = {
+--                 ["af"] = "@function.outer",
+--                 ["if"] = "@function.inner",
 
-                ["ac"] = "@conditional.outer",
-                ["ic"] = "@conditional.inner",
+--                 ["ac"] = "@conditional.outer",
+--                 ["ic"] = "@conditional.inner",
 
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
+--                 ["aa"] = "@parameter.outer",
+--                 ["ia"] = "@parameter.inner",
 
-                ["av"] = "@variable.outer",
-                ["iv"] = "@variable.inner",
-            },
-        },
-    },
-})
+--                 ["av"] = "@variable.outer",
+--                 ["iv"] = "@variable.inner",
+--             },
+--         },
+--     },
+-- })
--- common/nvim/.config/nvim/lua/psanker/plugins/treesitter.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/treesitter.lua
@@ -1,6 +1,79 @@
 return {
     {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+        config = function (_)
+            require('nvim-treesitter').setup({
+                auto_install = true,
+                sync_install = false,
+                ignore_install = {},
+                ensure_installed = {
+                    'r', 'rust', 'markdown',
+                    'markdown_inline', 'python',
+                    'go', 'ocaml', 'gitcommit'
+                },
+                highlight = {
+                    enable = true,
+                    disable = { 'tsql' },
+                },
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+
+                        goto_next_start = {
+                            ["]p"] = "@parameter.inner",
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                        },
+                        goto_next_end = {
+                            ["]M"] = "@function.outer",
+                            ["]["] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[p"] = "@parameter.inner",
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[M"] = "@function.outer",
+                            ["[]"] = "@class.outer",
+                        },
+                    },
+                    lsp_interop = {
+                        enable = true,
+                        border = 'none',
+                        peek_definition_code = {
+                            ["<leader>df"] = "@function.outer",
+                            ["<leader>dF"] = "@class.outer",
+                        },
+                    },
+                    select = {
+                        enable = true,
+                        lookahead = true,
+
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+
+                            ["ac"] = "@conditional.outer",
+                            ["ic"] = "@conditional.inner",
+
+                            ["aa"] = "@parameter.outer",
+                            ["ia"] = "@parameter.inner",
+
+                            ["av"] = "@variable.outer",
+                            ["iv"] = "@variable.inner",
+                        },
+                    },
+                },
+            })
+        end,
+    },
+    {
         'stevearc/aerial.nvim',
+        commit = 'da0ceef62eb58b9bec1975017beb2f28c3b1e72c',
         config = function(_)
             require("aerial").setup({
                 -- optionally use on_attach to set keymaps when aerial has attached to a buffer
