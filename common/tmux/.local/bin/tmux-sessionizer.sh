c6111f27eebe9b40d383c4356654ef46caba9c07
Patrick Anker <patricksanker@gmail.com>
2026-03-25 14:59:07 -0400

    feat: add sessionizer support for worktrees

--- common/tmux/.local/bin/tmux-sessionizer.sh
+++ common/tmux/.local/bin/tmux-sessionizer.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+WORKTREE=0
+
 if [ $# -eq 1 ]; then
   selected=$1
 else
-  selected=$(find $HOME/workspace $HOME/personal $HOME/Documents -type d -mindepth 1 -maxdepth 1 | fzf)
+    cmd="find $HOME/workspace $HOME/personal $HOME/Documents"
+
+    dirs_wt_roots=$(${cmd} -mindepth 2 -maxdepth 2 -type d | grep '**/.bare$')
+    dirs_wt_roots=$(echo "$dirs_wt_roots" | sed 's:/.bare::g')
+    dirs_wt=$(find $dirs_wt_roots -mindepth 1 -maxdepth 1 -type d -not -path '*/.*')
+    arr_wt=($dirs_wt)
+    
+    dirs_nowt=$(${cmd} -mindepth 1 -maxdepth 1 -type d | grep '**')
+
+    selected=$(printf "$dirs_wt\n$dirs_nowt" | fzf)
+
+    for it in "${arr_wt[@]}"; do
+        if [[ "$it" == "$selected" ]]; then
+            WORKTREE=1
+        fi
+    done
 fi
 
 echo "$selected"
@@ -12,7 +29,13 @@ if [ -z $selected ]; then
   exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
+if [ $WORKTREE -eq 1 ]; then
+    selected_name="$(basename $(dirname "$selected"))|$(basename "$selected")"
+else
+    selected_name=$(basename "$selected")
+fi
+
+selected_name=$(echo "$selected_name" | tr . _)
 open_tmux=$(pgrep tmux)
 
 if [ -z $TMUX ] && [ -z $open_tmux ]; then
