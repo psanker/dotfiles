6cbe7be9d855003e05220087159e5e236462d586
Patrick Anker <psanker@proton.me>
2026-06-29 10:11:18 -0400

    fix: pin treesitter for Neovim 0.11 compat
    Until I can safely update Odyssey

--- common/nvim/.config/nvim/lua/psanker/plugins/treesitter.lua
+++ common/nvim/.config/nvim/lua/psanker/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
+        commit = '90cd6580e720caedacb91fdd587b747a6e77d61f',
         build = ':TSUpdate',
         config = function (_)
             require('nvim-treesitter').setup({
